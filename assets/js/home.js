7b542de2b0f52218b92887e35402314be6b7021f
jarofexistence <cjosepht@icloud.com>
2021-11-22 13:25:24 -0500

    update

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const d = new Date();
+const birthDate = 20060809;
+var currentDateRange = configureDate(d) - birthDate;
+var firstText = document.getElementById("firstText");
+
+function configureDate(date) {
+    var toReturn = null;
+
+    toReturn = date.getFullYear() * 100;
+    toReturn = (toReturn + d.getMonth() + 1) * 100;
+    toReturn += date.getDate();
+
+    return toReturn;
+}
+
+function getAge(dateRange) {
+    var mathDate = dateRange / 10000;
+    mathDate = Math.floor(mathDate);
+
+    return mathDate;
+}
+
+var colinAge = getAge(currentDateRange);
+
+firstText.innerHTML = firstText.innerHTML.replace("[{1}]", colinAge);
