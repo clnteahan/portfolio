169f0d951becc1c4fd6e865c57859a5dc8fd7201
clnteahan <24cteahan@xbhs.net>
2021-12-15 13:43:58 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import {GoFileCode} from "react-icons/go";
+import './assets/css/home.css';
+import './assets/js/home.js';
+
+class Region1 extends React.Component {
+  render() {
+    return (
+      <div id="region1">
+        <div id="introText">Hello I'm Colin.</div>
+        <div id="firstText">Hello I am Colin Teahn, a **1** year old software developer
+          <p></p>
+          <div id="prefLanguages">Preffered Languages: <span id="cSharpBubble">C#</span> <span id="htmlBubble">Html</span> <span id="cssBubble">Css</span> <span id="jsBubble">Js</span> <span id="javaBubble">Java</span> <span id="pythonBubble">Python</span> </div>
+        </div>
+        <img id="img1" width="850.53901" height="740.82953" src="https://raw.githubusercontent.com/clnteahan/portfolio/main/assets/images/undraw_developer_activity_re_39tg.svg" alt="error: not found"></img>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Region1 />
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ start.bat
@@ -0,0 +1,4 @@
+@echo off
+cd "C:\react\main"
+npm start
+pause
